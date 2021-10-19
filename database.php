c5b03051cc7dc68dfa8db14171e4dee1615dac30
freeborn-coder <freebornganani@gmail.com>
2021-10-19 08:36:37 +0100

    show database errors

--- database.php
+++ database.php
@@ -4,7 +4,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASSWORD','');
-define('DB_DATABASE','upwork-json');
+define('DB_DATABASE','vincent_db');
 
 // create connection using the constants
 $db = new mysqli(DB_HOST,DB_USER, DB_PASSWORD, DB_DATABASE);
